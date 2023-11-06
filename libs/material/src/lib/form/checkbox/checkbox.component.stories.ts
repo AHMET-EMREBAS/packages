fab36bf1f9df2ef170685f2e1bc608b8d13da0f5
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-11-05 18:23:58 -0600

    storybook setup

--- libs/material/src/lib/form/checkbox/checkbox.component.stories.ts
+++ libs/material/src/lib/form/checkbox/checkbox.component.stories.ts
@@ -1,12 +1,21 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 import { CheckboxComponent } from './checkbox.component';
 
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CommonModule } from '@angular/common';
+import { provideFormGroup } from '../../api';
 
 const meta: Meta<CheckboxComponent> = {
   component: CheckboxComponent,
   title: 'CheckboxComponent',
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, MatCheckboxModule],
+      providers: [provideFormGroup({ active: false }), ],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<CheckboxComponent>;
